7ca71a8bc6990c25e73802c5aa91370c0f94aab2
qasem tous <145436@ppu.edu.ps>
2020-02-24 03:25:49 +0200

    if condition

--- prime_number.py
+++ prime_number.py
@@ -3,6 +3,11 @@
 counter = 0
 for number in range(1,1000):
     for i in range(2,number):
-
-
+        if number % i == 0:
+            break
+        i =i+1
+    else:
+        print(str(number))
+        counter =counter +1
         
+
