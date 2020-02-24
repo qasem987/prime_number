7617e7428b297a593eb3738e479608df5ebc7b6b
qasem tous <145436@ppu.edu.ps>
2020-02-24 03:27:38 +0200

    final programm

--- prime_number.py
+++ prime_number.py
@@ -9,5 +9,7 @@ for number in range(1,1000):
     else:
         print(str(number))
         counter =counter +1
-        
+
+print("the number of prime numbers between 1 and 1000 is :" + str(counter))
+
 
